ac2929ce3f9027e1f0a55394218fd708c5f3a5bf
Lucas Zins <zins.lucas@gmail.com>
2022-03-11 11:43:04 +0100

    push

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,85 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "Server.hpp"
+#include "Client.hpp"
+#include "utils.hpp"
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <sys/event.h>
-#include <err.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "Message.hpp"
 
+typedef std::vector<Client *>::iterator client_iterator;
 
-void error(const char *msg)
-{
-	perror(msg);
-	exit(0);
+Server::Server(std::string const & password): password(password) {}
+
+bool Server::try_password(std::string const & password) const {
+  return password == this->password;
 }
-int main(int argc, char *argv[])
-{
-	struct	kevent event;	 /* Event we want to monitor */
-	struct	kevent tevent;
-	int sockfd, newsockfd, portno, clilen, n, kq, ret;
+void Server::send_message(Client const *, std::string const & message_str) const {
+  int n = write(client->sockfd, message_str.c_str(), message_str.size());
+	if (n < 0)
+		throw SocketWriteError();
+}
 
-	char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	printf("sockfd = %i\n",sockfd);
-	if (sockfd < 0)
-		error("ERROR opening socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = atoi(argv[1]);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
-	if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
-	listen(sockfd,5);
-	clilen = sizeof(cli_addr);
-	kq = kqueue();
-	EV_SET(&event, sockfd, EVFILT_READ, EV_ADD | EV_ENABLE  , 0, 0, 0);
+void Server::welcome(Client const *) const;
 
-	printf("before ident = %i \n",tevent.ident);
-	ret = kevent(kq,&event, 1, NULL,0,NULL);
-	while (1)
-	{
-		 Message m;
-		// newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
-		//
-		//
-		printf("port = %i \n",cli_addr.sin_port);
-		ret = kevent(kq, &event, 1, &tevent,	1, NULL);
-		printf("ident = %i \n",tevent.ident);
-		if (tevent.flags & EV_EOF)
-            {
-                printf("Client has disconnected");
-                close(tevent.ident);
-            }
-		if (tevent.ident == sockfd)
-		{
-			newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
-			EV_SET(&event, newsockfd, EVFILT_READ, EV_ADD | EV_ENABLE  , 0, 0, 0);
-		}
-		else
-		{
-			bzero(buffer,256);
-			// n = read(tevent.ident,buffer,255);
-			n = recv(newsockfd,buffer,255,0);
-			if (n < 0)
-				error("ERROR reading from socket");
+static struct SameNick {
+  bool operator()(std::string const & nick, Client const * client) {
+    return nick == client->nick;
+  }
+};
 
-			printf("Here is the message: %s\n",buffer);
-			m.parse(buffer);
-			std::cout<<m;
-			n = write(newsockfd,"I got your message",18);
-			if (n < 0)
-				error("ERROR writing to socket");
-		}
-	}
-	return 0;
+Server::nick_exists(std::string const & nick) const {
+  client_iterator it = find_in_vector<SameNick>(nick, this->clients);
+  return (it != this->clients.end());
 }
---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/event.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Message.hpp"
+
+
+void error(const char *msg)
+{
+	perror(msg);
+	exit(0);
+}
+int main(int argc, char *argv[])
+{
+	struct	kevent event;	 /* Event we want to monitor */
+	struct	kevent tevent;
+	int sockfd, newsockfd, portno, clilen, n, kq, ret;
+
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	printf("sockfd = %i\n",sockfd);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+		error("ERROR on binding");
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+	kq = kqueue();
+	EV_SET(&event, sockfd, EVFILT_READ, EV_ADD | EV_ENABLE  , 0, 0, 0);
+
+	printf("before ident = %i \n",tevent.ident);
+	ret = kevent(kq,&event, 1, NULL,0,NULL);
+	while (1)
+	{
+		 Message m;
+		// newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
+		//
+		//
+		printf("port = %i \n",cli_addr.sin_port);
+		ret = kevent(kq, &event, 1, &tevent,	1, NULL);
+		printf("ident = %i \n",tevent.ident);
+		if (tevent.flags & EV_EOF)
+            {
+                printf("Client has disconnected");
+                close(tevent.ident);
+            }
+		if (tevent.ident == sockfd)
+		{
+			newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
+			EV_SET(&event, newsockfd, EVFILT_READ, EV_ADD | EV_ENABLE  , 0, 0, 0);
+		}
+		else
+		{
+			bzero(buffer,256);
+			// n = read(tevent.ident,buffer,255);
+			n = recv(newsockfd,buffer,255,0);
+			if (n < 0)
+				error("ERROR reading from socket");
+
+			printf("Here is the message: %s\n",buffer);
+			m.parse(buffer);
+			std::cout<<m;
+			n = write(newsockfd,"I got your message",18);
+			if (n < 0)
+				error("ERROR writing to socket");
+		}
+	}
+	return 0;
+}
